3319756effa127227e74abfc9b9b5cf882f1041a
Michael Blydenburgh <mike.blydenburgh@gmail.com>
2025-08-27 06:24:47 -0400

    return 200

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
+use std::io::Write;
 #[allow(unused_imports)]
 use std::net::TcpListener;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+fn construct_response() -> Result<String, anyhow::Error> {
+    let status = String::from("HTTP/1.1 200 OK\r\n");
+    let headers = String::from("\r\n");
+    let body = String::from("\r\n");
+    Ok(status + &headers + &body)
+}
 
-    // Uncomment this block to pass the first stage
-    //
+fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                //let reader = BufReader::new(&stream);
+                if let Ok(res_str) = construct_response() {
+                    let response = res_str.as_bytes();
+                    stream.write_all(response);
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
