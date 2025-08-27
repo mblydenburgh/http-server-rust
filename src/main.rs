8dfbe300d5b9c4ae9ab76064211ac28879294364
Michael Blydenburgh <mike.blydenburgh@gmail.com>
2025-08-27 06:56:28 -0400

    log incoming request

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use std::io::Write;
+use std::io::{BufRead, BufReader, Read, Write};
 #[allow(unused_imports)]
 use std::net::TcpListener;
 
-fn construct_response() -> Result<String, anyhow::Error> {
-    let status = String::from("HTTP/1.1 200 OK\r\n");
+#[derive(Debug)]
+enum StatusCode {
+    Ok(String),
+    NotFound(String),
+}
+
+fn construct_response(code: StatusCode) -> Result<String, anyhow::Error> {
+    let status = String::from(format!("HTTP/1.1 {:?} OK\r\n", code));
     let headers = String::from("\r\n");
     let body = String::from("\r\n");
     Ok(status + &headers + &body)
@@ -15,8 +21,16 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                //let reader = BufReader::new(&stream);
-                if let Ok(res_str) = construct_response() {
+                let reader = BufReader::new(&stream);
+                let request: Vec<_> = reader
+                    .lines()
+                    .map(|line| line.unwrap())
+                    .take_while(|line| !line.is_empty())
+                    .collect();
+                for line in request {
+                    println!("Line: {}", line);
+                }
+                if let Ok(res_str) = construct_response(StatusCode::Ok("200 OK".into())) {
                     let response = res_str.as_bytes();
                     stream.write_all(response);
                 }
