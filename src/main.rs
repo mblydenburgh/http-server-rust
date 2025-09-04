1dfd037fab0a62d8a6e272f747b1ff867089c6cd
Michael Blydenburgh <mike.blydenburgh@gmail.com>
2025-09-04 19:23:05 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,18 @@
 use std::net::TcpListener;
 use std::{
     fs,
-    io::{BufRead, BufReader, Read, Write},
+    io::{Read, Write},
     net::TcpStream,
+    path::PathBuf,
 };
 
 #[derive(Debug)]
 enum StatusCode {
     Ok(),
+    Created(),
     NotFound(),
+    MethodNotAllowed(),
+    ServerError(),
 }
 
 impl std::fmt::Display for StatusCode {
@@ -22,7 +26,10 @@ impl StatusCode {
     fn as_string(&self) -> String {
         match self {
             StatusCode::Ok() => "200 OK".into(),
+            StatusCode::Created() => "201 Created".into(),
             StatusCode::NotFound() => "404 Not Found".into(),
+            Self::MethodNotAllowed() => "405 Method Not Allowed".into(),
+            Self::ServerError() => "500 Server Error".into(),
         }
     }
 }
@@ -77,12 +84,16 @@ fn construct_response(
     Ok(status + "\r\n" + &headers_str + "\r\n" + &body)
 }
 
-fn parse_request(stream: &TcpStream) -> Result<RawHttpRequest, anyhow::Error> {
-    let mut reader = BufReader::new(stream);
-    let mut request_line = String::new();
-    reader.read_line(&mut request_line)?;
+fn parse_request(mut stream: &TcpStream) -> Result<RawHttpRequest, anyhow::Error> {
+    let mut buffer = [0; 4096];
+    let req_bytes = stream.read(&mut buffer).expect("could not read stream");
+    if req_bytes == 0 {
+        return Err(anyhow::anyhow!("could not read request bytes"));
+    }
+    let req_str = String::from_utf8_lossy(&buffer[..req_bytes]);
+    let mut lines = req_str.lines();
+    let request_line = lines.next().ok_or(anyhow::anyhow!("no request line"))?;
     let request_line_parts: Vec<String> = request_line
-        .clone()
         .split_whitespace()
         .map(|s| s.to_string())
         .collect();
@@ -91,19 +102,37 @@ fn parse_request(stream: &TcpStream) -> Result<RawHttpRequest, anyhow::Error> {
     }
 
     let mut headers: Vec<Header> = vec![];
+    let mut content_length = 0;
 
-    loop {
-        let mut line = String::new();
-        reader.read_line(&mut line)?;
-        if line.starts_with("\r\n") {
+    for line in lines.by_ref() {
+        if line.is_empty() {
             break;
         }
         let header_parts: Vec<&str> = line.split_whitespace().collect();
+        println!("header parts: {:?}", header_parts);
         headers.push(Header {
             key: header_parts[0].replace(":", "").into(),
             value: header_parts[1].into(),
         });
+        if header_parts[0].eq_ignore_ascii_case("content-length") {
+            content_length = header_parts[1].parse().unwrap_or(0);
+        }
     }
+
+    let body_start_index = req_str.find("\r\n\r\n").unwrap_or(0) + 4;
+    println!("body start: {}", body_start_index);
+    let body = if body_start_index > request_line.len() {
+        req_str[body_start_index..].to_string()
+    } else {
+        String::new()
+    };
+    println!("parsed body: {}", body);
+
+    let body = if content_length > 0 && body.len() > content_length {
+        body[..content_length].to_string()
+    } else {
+        body
+    };
 
     let mut path_parts: Vec<String> = request_line_parts[1]
         .clone()
@@ -117,7 +146,7 @@ fn parse_request(stream: &TcpStream) -> Result<RawHttpRequest, anyhow::Error> {
         path_parts,
         version: String::from(&request_line_parts[2]),
         headers,
-        body: String::new(),
+        body,
     })
 }
 
@@ -150,7 +179,11 @@ fn handle_request(mut stream: TcpStream) {
     let response = match root.as_str() {
         "echo" => echo(request),
         "user-agent" => user_agent(request),
-        "files" => file(request),
+        "files" => match request.method.as_str() {
+            "GET" => get_file(request),
+            "POST" => post_file(request),
+            _ => construct_response(StatusCode::MethodNotAllowed(), None, None),
+        },
         "" => construct_response(StatusCode::Ok(), None, None),
         _ => construct_response(StatusCode::NotFound(), None, None),
     };
@@ -158,13 +191,16 @@ fn handle_request(mut stream: TcpStream) {
     if let Ok(res_str) = response {
         let response = res_str.as_bytes();
         let result = stream.write_all(response);
-        let _ = stream.flush();
         if let Err(e) = result {
-            panic!("ahhh! {:?}?", e);
+            let err = construct_response(StatusCode::ServerError(), None, Some(e.to_string()));
+            let _ = stream.write_all(err.unwrap().as_bytes());
         }
     } else {
-        println!("could not build response");
+        let err = construct_response(StatusCode::ServerError(), None, None);
+        let _ = stream.write_all(err.unwrap().as_bytes());
     }
+    let _ = stream.flush();
+    let _ = stream.shutdown(std::net::Shutdown::Both);
 }
 
 fn echo(request: RawHttpRequest) -> Result<String, anyhow::Error> {
@@ -199,16 +235,15 @@ fn user_agent(request: RawHttpRequest) -> Result<String, anyhow::Error> {
     )
 }
 
-fn file(request: RawHttpRequest) -> Result<String, anyhow::Error> {
+fn get_file(request: RawHttpRequest) -> Result<String, anyhow::Error> {
     let file_name = format!("{}", request.path_parts[1].clone());
+    // get directory from ./your_program.sh --directory /tmp/
     let argv = std::env::args().collect::<Vec<String>>();
     let dir = argv[2].clone();
     if let Ok(mut file) = fs::File::open(format!("{dir}{file_name}")) {
-        println!("found");
         let mut body = String::new();
         file.read_to_string(&mut body).unwrap();
         let content_length = body.len().to_string();
-        println!("body: {}", body);
         construct_response(
             StatusCode::Ok(),
             Some(vec![
@@ -218,7 +253,38 @@ fn file(request: RawHttpRequest) -> Result<String, anyhow::Error> {
             Some(body),
         )
     } else {
-        println!("not found");
         construct_response(StatusCode::NotFound(), None, None)
+    }
+}
+
+fn post_file(request: RawHttpRequest) -> Result<String, anyhow::Error> {
+    println!("posting file {}", request.path_parts[1].clone());
+    println!("body contents: {}", request.body);
+    let file_name = format!("{}", request.path_parts[1].clone());
+    // get directory from ./your_program.sh --directory /tmp/some/dir/
+    let argv = std::env::args().collect::<Vec<String>>();
+    let dir = argv[2].clone(); // /tmp/some/dir/
+
+    let name = format!("{dir}{file_name}");
+
+    let body_length = request
+        .headers
+        .iter()
+        .find(|h| h.key == "Content-Length")
+        .expect("No Content-Length header")
+        .value
+        .clone()
+        .parse::<usize>()
+        .unwrap();
+
+    match fs::write(name, &request.body.as_bytes()) {
+        Ok(_) => {
+            println!("wrote file");
+            construct_response(StatusCode::Created(), None, None)
+        }
+        Err(_) => {
+            println!("could not write file");
+            construct_response(StatusCode::ServerError(), None, None)
+        }
     }
 }
